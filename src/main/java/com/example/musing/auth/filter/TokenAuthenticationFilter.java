bfc5af153a430dcfd0edf538244b4dc0e38e35a7
JTH-00 <dbzgtsa@gmail.com>
2024-12-05 17:50:48 +0900

    feat(JTH-00): 토큰 발급 관련 필터 추가

--- src/main/java/com/example/musing/auth/filter/TokenAuthenticationFilter.java
+++ src/main/java/com/example/musing/auth/filter/TokenAuthenticationFilter.java
@@ -1,18 +1,62 @@
 package com.example.musing.auth.filter;
 
+import com.example.musing.auth.JWT.TokenProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Component
+@RequiredArgsConstructor
 public class TokenAuthenticationFilter extends OncePerRequestFilter {
+    private final TokenProvider tokenProvider;
+
     //엑세스 토큰 및 리프래쉬 토큰 확인
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        //미작성
+        String accessToken = resolveToken(request);
+
+        //엑세스 토큰 검증
+        if (tokenProvider.validateToken(accessToken)) {
+            setAuthentication(accessToken);
+        } else {
+            // 만료되었을 경우 accessToken 재발급
+            String reissueAccessToken = tokenProvider.reissueAccessToken(accessToken);
+
+            if (StringUtils.hasText(reissueAccessToken)) {
+                setAuthentication(reissueAccessToken);
+
+                // 재발급된 accessToken 다시 전달
+                response.setHeader(AUTHORIZATION, "Bearer " + reissueAccessToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(String accessToken) {
+        //시큐리티 context에 등록할 Authentication 생성 및 등록
+        Authentication authentication = tokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String token = request.getHeader(AUTHORIZATION);
+        if (ObjectUtils.isEmpty(token) || !token.startsWith("Bearer ")) {
+            return null;
+        }
+        return token.substring("Bearer ".length());
     }
 }
