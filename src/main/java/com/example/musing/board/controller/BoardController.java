4eedf7392d19ccec67ea3333b8579a684235321c
JTH-00 <dbzgtsa@gmail.com>
2025-01-19 01:44:26 +0900

    chore : 병합 누락된 부분 수정

--- src/main/java/com/example/musing/board/controller/BoardController.java
+++ src/main/java/com/example/musing/board/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.example.musing.board.controller;
 
-import com.example.musing.board.dto.BoardAndReplyPageDto;
 import com.example.musing.board.dto.BoardListRequestDto;
 import com.example.musing.board.dto.CreateBoardRequest;
 import com.example.musing.board.dto.UpdateBoardRequestDto;
@@ -10,9 +9,7 @@ import com.example.musing.common.dto.ResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -21,13 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class BoardController {
 
     private final BoardService boardService;
-  
-    @PostMapping("/create")
-    public BoardController(BoardService boardService) {
-        this.boardService = boardService;
-    }
 
-    @PostMapping(value ="/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseDto<Board> createPost(@RequestBody @Valid CreateBoardRequest request) {
         boardService.createBoard(request); // DTO를 Service로 전달
         return ResponseDto.of(null, "성공적으로 글이 작성되었습니다.");
@@ -55,20 +47,14 @@ public class BoardController {
         return ResponseDto.of(responseList);
     }
 
-    @GetMapping(value ="/selectAll", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<CreateBoardResponse>> getAllBoards() {
-        List<CreateBoardResponse> responseList = boardService.getAllBoards();
-        return ResponseEntity.ok(responseList);
-    }
-
-    @PutMapping(value ="/updatePost", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseDto<Board> updatePost(@RequestParam("boardId") Long boardId,@RequestBody UpdateBoardRequestDto updateRequest){
-        boardService.updateBoard(boardId,updateRequest);
-        return ResponseDto.of(null,"성공적으로 글이 수정되었습니다.");
+    @PutMapping(value = "/updatePost", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseDto<Board> updatePost(@RequestParam("boardId") Long boardId, @RequestBody UpdateBoardRequestDto updateRequest) {
+        boardService.updateBoard(boardId, updateRequest);
+        return ResponseDto.of(null, "성공적으로 글이 수정되었습니다.");
     }
 
     @DeleteMapping("/deletePost")
-    public ResponseDto<Board> deletePost(@RequestParam("boardId") Long boardId){
+    public ResponseDto<Board> deletePost(@RequestParam("boardId") Long boardId) {
         boardService.deleteBoard(boardId);
         return ResponseDto.of(null, "성공적으로 글이 삭제되었습니다.");
     }
