0e7cd3154be4a1914d1630268029f06aaa205939
JTH-00 <dbzgtsa@gmail.com>
2025-01-21 00:23:46 +0900

    chore : 스웨거 문서 수정 및 추가 작성

--- src/main/java/com/example/musing/genre/dto/GenreDto.java
+++ src/main/java/com/example/musing/genre/dto/GenreDto.java
@@ -2,11 +2,14 @@ package com.example.musing.genre.dto;
 
 import com.example.musing.genre.entity.Genre;
 import com.example.musing.genre.entity.Genre_Music;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record GenreDto(
+        @Schema(description = "장르 ID", example = "1")
         long id,
+        @Schema(description = "장르명", example = "락")
         String genreName
 ) {
     public static GenreDto toDto(Genre_Music genreMusic){
