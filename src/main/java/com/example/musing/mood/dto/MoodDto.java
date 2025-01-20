80e3d5dd47d5d58fb87b46a7e887c9fec447c1a6
JTH-00 <dbzgtsa@gmail.com>
2025-01-21 00:23:46 +0900

    chore : 스웨거 문서 수정 및 추가 작성

--- src/main/java/com/example/musing/mood/dto/MoodDto.java
+++ src/main/java/com/example/musing/mood/dto/MoodDto.java
@@ -2,11 +2,14 @@ package com.example.musing.mood.dto;
 
 import com.example.musing.mood.entity.Mood;
 import com.example.musing.mood.entity.Mood_Music;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record MoodDto(
+        @Schema(description = "분위기 ID", example = "1")
         long id,
+        @Schema(description = "분위기명", example = "잔잔한")
         String moodName
 ) {
     public static MoodDto toDto(Mood_Music moodMusic) {
