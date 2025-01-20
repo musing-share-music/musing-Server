52d56e37a455486e1a8483a2cba6769062f18349
JTH-00 <dbzgtsa@gmail.com>
2025-01-21 00:23:46 +0900

    chore : 스웨거 문서 수정 및 추가 작성

--- src/main/java/com/example/musing/artist/dto/ArtistDto.java
+++ src/main/java/com/example/musing/artist/dto/ArtistDto.java
@@ -1,11 +1,14 @@
 package com.example.musing.artist.dto;
 
 import com.example.musing.artist.entity.Artist;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record ArtistDto (
+        @Schema(description = "아티스트 ID", example = "1")
         long id,
+        @Schema(description = "아티스트명", example = "아티스트명")
         String name){
     public static ArtistDto toDto(Artist artist){
         return ArtistDto.builder()
