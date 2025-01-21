ff00a1f880a35260016ef89677dbbc0e9110fe77
JTH-00 <dbzgtsa@gmail.com>
2025-01-22 01:09:37 +0900

    refactor : 스웨거 작성하면서 생긴 오류 수정, 메인 반영 예정

--- src/main/java/com/example/musing/user/entity/User.java
+++ src/main/java/com/example/musing/user/entity/User.java
@@ -98,4 +98,7 @@ public class User { //https://developers.google.com/identity/openid-connect/open
     public void updateactivated(Boolean activated){
         this.activated = activated;
     }
+    public void updateactivated(Boolean activated){
+        this.activated = activated;
+    }
 }
