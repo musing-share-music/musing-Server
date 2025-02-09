d9f092a3c686d96c299808aa4d2a9d51e8e59cc1
JTH-00 <dbzgtsa@gmail.com>
2025-02-10 01:26:12 +0900

    chore : 병합 충돌 다시 체크

--- src/main/java/com/example/musing/user/entity/User.java
+++ src/main/java/com/example/musing/user/entity/User.java
@@ -95,10 +95,4 @@ public class User { //https://developers.google.com/identity/openid-connect/open
     public void updateactivated(Boolean activated) {
         this.activated = activated;
     }
-    public void updateactivated(Boolean activated){
-        this.activated = activated;
-    }
-    public void updateactivated(Boolean activated){
-        this.activated = activated;
-    }
 }
