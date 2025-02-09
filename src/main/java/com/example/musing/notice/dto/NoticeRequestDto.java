9eff600251a0ef42451e2699b36497b91977b81a
JTH-00 <dbzgtsa@gmail.com>
2025-02-10 02:07:16 +0900

    chore : 공지사항 관련 스웨거 추가 작성

--- src/main/java/com/example/musing/notice/dto/NoticeRequestDto.java
+++ src/main/java/com/example/musing/notice/dto/NoticeRequestDto.java
@@ -1,9 +1,12 @@
 package com.example.musing.notice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record NoticeRequestDto(
+        @Schema(description = "공지사항 제목", example = "안녕하세요. 음악 추천 사이트 Musing입니다.")
         String title,
+        @Schema(description = "공지사항 내용", example = "여러분들의 다양한 음악 서비스를 제공하기 위해 현재 ....")
         String content) {
 }
