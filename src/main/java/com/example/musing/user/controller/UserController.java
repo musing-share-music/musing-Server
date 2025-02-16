1c3d89d2bee190dfe80ace8482471a8cdfa5d6ec
JTH-00 <dbzgtsa@gmail.com>
2025-02-16 21:32:25 +0900

    feat : 회원 정보 모달류 수정 기능 추가

--- src/main/java/com/example/musing/user/controller/UserController.java
+++ src/main/java/com/example/musing/user/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.example.musing.user.controller;
 
+import com.example.musing.artist.dto.ArtistDto;
 import com.example.musing.common.dto.ResponseDto;
+import com.example.musing.genre.dto.GenreDto;
+import com.example.musing.mood.dto.MoodDto;
 import com.example.musing.user.dto.UserResponseDto;
 import com.example.musing.user.entity.User;
 import com.example.musing.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/musing/user")
 @RequiredArgsConstructor
@@ -21,6 +24,21 @@ public class UserController {
     @GetMapping
     public ResponseDto<UserResponseDto.UserInfoPageDto> getUserInfoPage() {
         return ResponseDto.of(userService.getUserInfoPage(getUserId()));
+    }
+
+    @PostMapping("/genre")
+    public ResponseDto<List<GenreDto>> updateGenres(@RequestBody List<Long> chooseGenres) {
+        return ResponseDto.of(userService.updateGenres(getUserId(), chooseGenres));
+    }
+
+    @PostMapping("/mood")
+    public ResponseDto<List<MoodDto>> updateMoods(@RequestBody List<Long> chooseMoods) {
+        return ResponseDto.of(userService.updateMoods(getUserId(), chooseMoods));
+    }
+
+    @PostMapping("/artist")
+    public ResponseDto<List<ArtistDto>> updateArtists(@RequestBody List<String> chooseArtist) {
+        return ResponseDto.of(userService.updateArtists(getUserId(), chooseArtist));
     }
 
     private User getUserId() {
--- src/main/java/com/example/musing/user/repository/User_LikeArtistRepository.java
+++ src/main/java/com/example/musing/user/repository/User_LikeArtistRepository.java
@@ -2,9 +2,13 @@ package com.example.musing.user.repository;
 
 import com.example.musing.user.entity.User;
 import com.example.musing.user.entity.User_LikeArtist;
+import com.example.musing.user.entity.User_LikeMood;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface User_LikeArtistRepository extends JpaRepository<User_LikeArtist, Long> {
     boolean existsByUser(User user);
 
+    List<User_LikeArtist> findByUser(User user);
 }
