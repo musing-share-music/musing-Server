4e917daa75281284ce6d6070d05ead1741078f81
JTH-00 <dbzgtsa@gmail.com>
2025-02-16 21:51:26 +0900

    chore : 2025-02-16 회원 정보 조회 모달류 수정 기능 스웨거 작성

--- src/main/java/com/example/musing/user/controller/UserController.java
+++ src/main/java/com/example/musing/user/controller/UserController.java
@@ -31,16 +31,25 @@ public class UserController {
         return ResponseDto.of(userService.getUserInfoPage(getUserId()));
     }
 
+    @Operation(summary = "유저가 선호하는 장르 수정",
+            description = "유저가 선호하는 장르의 수정된 결과의 Id값을 리스트로 주면 됩니다.<br>" +
+                    "삭제하고 추가한 것을 구분없이 전체 결과로 보내면 됩니다.")
     @PostMapping("/genre")
     public ResponseDto<List<GenreDto>> updateGenres(@RequestBody List<Long> chooseGenres) {
         return ResponseDto.of(userService.updateGenres(getUserId(), chooseGenres));
     }
 
+    @Operation(summary = "유저가 선호하는 분위기 수정",
+            description = "유저가 선호하는 분위기의 수정된 결과의 Id값을 리스트로 주면 됩니다.<br>" +
+                    "삭제하고 추가한 것을 구분없이 전체 결과로 보내면 됩니다.")
     @PostMapping("/mood")
     public ResponseDto<List<MoodDto>> updateMoods(@RequestBody List<Long> chooseMoods) {
         return ResponseDto.of(userService.updateMoods(getUserId(), chooseMoods));
     }
 
+    @Operation(summary = "유저가 선호하는 가수 수정",
+            description = "유저가 선호하는 가수의 수정된 결과의 String값을 리스트로 주면 됩니다.<br>" +
+                    "삭제하고 추가한 것을 구분없이 전체 결과로 보내면 됩니다.")
     @PostMapping("/artist")
     public ResponseDto<List<ArtistDto>> updateArtists(@RequestBody List<String> chooseArtist) {
         return ResponseDto.of(userService.updateArtists(getUserId(), chooseArtist));
