df0a83348e28b43fdd8715158ae51779b21d789a
JTH-00 <dbzgtsa@gmail.com>
2025-02-21 02:11:15 +0900

    chore : 회원 정보 조회 관련 기능 전체 추가

--- src/main/java/com/example/musing/reply/dto/ReplyResponseDto.java
+++ src/main/java/com/example/musing/reply/dto/ReplyResponseDto.java
@@ -2,17 +2,24 @@ package com.example.musing.reply.dto;
 
 import com.example.musing.music.dto.MusicDto;
 import com.example.musing.reply.entity.Reply;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
 public record ReplyResponseDto(
+        @Schema(description = "리뷰의 Id")
         Long id,
+        @Schema(description = "리뷰의 별점", example = "5.0")
         long starScore,
+        @Schema(description = "리뷰의 내용", example = "신나서 운동하면서 듣기 좋아요")
         String content,
+        @Schema(description = "작성한 리뷰의 음악 정보")
         MusicDto musicDto,
+        @Schema(description = "작성일자")
         LocalDateTime createdAt,
+        @Schema(description = "수정일자")
         LocalDateTime updatedAt
 
 ) {
