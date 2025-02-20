d0ea60ae020bcc52e8d3b92cf56cae2981dab2a6
JTH-00 <dbzgtsa@gmail.com>
2025-02-20 18:33:06 +0900

    feat : 회원 정보 수정에 필요한 모달창 조회 추가

--- src/main/java/com/example/musing/user/controller/UserController.java
+++ src/main/java/com/example/musing/user/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.musing.user.controller;
 import com.example.musing.artist.dto.ArtistDto;
 import com.example.musing.common.dto.ResponseDto;
 import com.example.musing.genre.dto.GenreDto;
+import com.example.musing.genre.service.GenreService;
 import com.example.musing.mood.dto.MoodDto;
+import com.example.musing.mood.service.MoodService;
 import com.example.musing.user.dto.UserResponseDto;
 import com.example.musing.user.entity.User;
 import com.example.musing.user.service.UserService;
@@ -20,15 +22,27 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+    private final GenreService genreService;
+    private final MoodService moodService;
 
     @GetMapping
     public ResponseDto<UserResponseDto.UserInfoPageDto> getUserInfoPage() {
         return ResponseDto.of(userService.getUserInfoPage(getUserId()));
     }
 
+    @GetMapping("/like/genre")
+    public ResponseDto<List<GenreDto>> selectGenreForm() {
+        return ResponseDto.of(genreService.getGenreDtos(), "");
+    }
+
     @PostMapping("/genre")
     public ResponseDto<List<GenreDto>> updateGenres(@RequestBody List<Long> chooseGenres) {
         return ResponseDto.of(userService.updateGenres(getUserId(), chooseGenres));
+    }
+
+    @GetMapping("/like/mood")
+    public ResponseDto<List<MoodDto>> selectMoodForm() {
+        return ResponseDto.of(moodService.getMoodDtos(), "");
     }
 
     @PostMapping("/mood")
@@ -37,7 +51,7 @@ public class UserController {
     }
 
     @PostMapping("/artist")
-    public ResponseDto<List<ArtistDto>> updateArtists(@RequestBody List<String> chooseArtist) {
+    public ResponseDto<List<ArtistDto>> updateArtists(@RequestBody(required = false) List<String> chooseArtist) {
         return ResponseDto.of(userService.updateArtists(getUserId(), chooseArtist));
     }
 
