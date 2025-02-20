1bb1aedd70a6b811a20acc2b589c3d1a143a3fed
JTH-00 <dbzgtsa@gmail.com>
2025-02-21 02:11:15 +0900

    chore : 회원 정보 조회 관련 기능 전체 추가

--- src/main/java/com/example/musing/music/dto/MusicDto.java
+++ src/main/java/com/example/musing/music/dto/MusicDto.java
@@ -5,6 +5,7 @@ import com.example.musing.artist.entity.Artist_Music;
 import com.example.musing.board.dto.BoardListResponseDto;
 import com.example.musing.board.entity.Board;
 import com.example.musing.music.entity.Music;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -12,8 +13,11 @@ import java.util.List;
 
 @Builder
 public record MusicDto(
+        @Schema(description = "노래 제목", example = "Manhattan")
         String musicName,
+        @Schema(description = "가수 목록")
         List<ArtistDto> artists,
+        @Schema(description = "해당 곡의 유튜브 썸네일 링크")
         String thumbNailLink
 ) {
     public static MusicDto toDto(Music music) {
