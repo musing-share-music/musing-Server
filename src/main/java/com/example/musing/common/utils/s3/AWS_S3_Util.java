57c0f13a22405a5211663a09b2e577a9516be262
JTH-00 <dbzgtsa@gmail.com>
2025-02-25 05:57:54 +0900

    chore : 오탈자 수정 및 @Transactional 누락 코드 수정

--- src/main/java/com/example/musing/common/utils/s3/AWS_S3_Util.java
+++ src/main/java/com/example/musing/common/utils/s3/AWS_S3_Util.java
@@ -1,12 +1,12 @@
-package com.example.musing.common.utils.s3;
+package com.example.musing.common.utils.S3;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.ion.IonException;
 
 import java.io.IOException;
 
@@ -17,13 +17,13 @@ public class AWS_S3_Util {
     private String bucketName; //버킷 이름
     private final AmazonS3 amazonS3;
 
-    //file : 업로드할 사진, useServiceName : S3버킷 경로(디렉토리 구조 따라갈 예정), fileName : 파일이름 (uuid 사용할 예정)
+    @Transactional
     public String uploadImageToS3(MultipartFile file, String useServiceName, String fileName){
         try{ //이미지를 S3에 업로드하고 이미지의 url을 반환
             ObjectMetadata objectMetadata = new ObjectMetadata();
             objectMetadata.setContentType(file.getContentType());
             PutObjectResult putObjectResult = amazonS3.putObject(new PutObjectRequest(
-                    bucketName,useServiceName+"/"+fileName, file.getInputStream(),objectMetadata
+                    bucketName,useServiceName + "/" + fileName, file.getInputStream(),objectMetadata
             ).withCannedAcl(CannedAccessControlList.PublicRead));//서버에 이미지 업로드
         } catch (IOException e) {
             throw new RuntimeException("이미지 업로드 중 오류 발생: " + e.getMessage());
@@ -31,6 +31,7 @@ public class AWS_S3_Util {
         return amazonS3.getUrl(bucketName,useServiceName + "/" + fileName).toString(); //데이터베이스에 저장할 이미지가 저장된 주소
     }
 
+    @Transactional
     public void deleteFile(String useServiceName, String filename){
         amazonS3.deleteObject(new DeleteObjectRequest(bucketName,useServiceName + "/" + filename));
     }
