459db01d17412085edc990c6a88233ac3d8a3a88
JTH-00 <dbzgtsa@gmail.com>
2025-03-02 19:35:20 +0900

    chore : 누락 어노테이션 추가

--- src/main/java/com/example/musing/auth/controller/AuthController.java
+++ src/main/java/com/example/musing/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.example.musing.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class AuthController {
     private final TokenService tokenService;
 
     @GetMapping("/auth/reissue")
-    public ResponseDto<String> reissueToken(String email) {
+    public ResponseDto<String> reissueToken(@RequestParam String email) {
         tokenService.reissueToken(email);
         return ResponseDto.of("", "토큰 재발급에 성공했습니다.");
     }
