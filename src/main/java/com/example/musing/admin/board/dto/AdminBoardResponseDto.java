86594488bfb82aa884e8c6fcd11094f1947c5e13
JTH-00 <dbzgtsa@gmail.com>
2025-03-16 20:28:26 +0900

    pix : 스웨거 스키마 인식 안되던 버그 확인 및 의존성 주입 안되던 부분 수정

--- src/main/java/com/example/musing/admin/board/dto/AdminBoardResponseDto.java
+++ src/main/java/com/example/musing/admin/board/dto/AdminBoardResponseDto.java
@@ -1,23 +1,19 @@
 package com.example.musing.admin.board.dto;
 
-import com.example.musing.artist.dto.ArtistDto;
 import com.example.musing.board.entity.Board;
-import com.example.musing.genre.dto.GenreDto;
-import com.example.musing.mood.dto.MoodDto;
-import com.example.musing.user.entity.User;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 public class AdminBoardResponseDto {
     @Builder
-    public record BoardListDto(
+    public record AdminBoardListDto(
             String title,
             String username,
             LocalDateTime createdAt
     ) {
-        public static BoardListDto toDto(Board board) {
-            return BoardListDto.builder()
+        public static AdminBoardListDto toDto(Board board) {
+            return AdminBoardListDto.builder()
                     .title(board.getTitle())
                     .username(board.getUser().getUsername())
                     .createdAt(board.getCreatedAt())
