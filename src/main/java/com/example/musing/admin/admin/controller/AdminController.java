c57f08b408984aed55b99a40e69caf4d839a5cf8
JTH-00 <dbzgtsa@gmail.com>
2025-03-29 12:24:42 +0900

    chore : 관리자 관련 스웨거 설명 추가

--- src/main/java/com/example/musing/admin/admin/controller/AdminController.java
+++ src/main/java/com/example/musing/admin/admin/controller/AdminController.java
@@ -8,12 +8,15 @@ import com.example.musing.exception.CustomException;
 import com.example.musing.exception.ErrorCode;
 import com.example.musing.user.dto.UserResponseDto;
 import com.example.musing.user.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "관리자 정보 조회 API")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/musing/admin")
@@ -21,6 +24,7 @@ public class AdminController {
 
     private final AdminService adminService;
 
+    @Operation(summary = "관리자 정보 조회")
     @GetMapping("/info")
     public ResponseDto<AdminInfoDto> getAdminInfo() {
         AdminInfoDto adminInfoDto = adminService.getUserInfo();
