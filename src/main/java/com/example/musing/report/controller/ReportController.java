c0cc76df15d2458476cc76430d095cbe8d3d3f03
JTH-00 <dbzgtsa@gmail.com>
2025-04-02 15:11:35 +0900

    chore: 스웨거 Tag 추가 및 알람 기능 작성

--- src/main/java/com/example/musing/report/controller/ReportController.java
+++ src/main/java/com/example/musing/report/controller/ReportController.java
@@ -5,10 +5,12 @@ import com.example.musing.notice.dto.NoticeDto;
 import com.example.musing.report.dto.ReportRequestDto;
 import com.example.musing.report.dto.ReportResponseDto;
 import com.example.musing.report.service.ReportService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "신고 관련 API (관리자 기능 포함)")
 @RequestMapping("/musing")
 @RequiredArgsConstructor
 @RestController
