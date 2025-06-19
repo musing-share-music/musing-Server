4d60388aa06af7b9122523f4ed50e21d2d0c4878
ASUS\gkrua <gkrua5056@naver.com>
2025-06-19 21:41:35 +0900

    기본이미지 추가

--- src/main/java/com/example/musing/playlist/service/PlaylistServiceImpl.java
+++ src/main/java/com/example/musing/playlist/service/PlaylistServiceImpl.java
@@ -366,7 +366,7 @@ public class PlaylistServiceImpl implements PlaylistService {
             }
             return title;
         } catch (Exception e) {
-            return "제목을 찾을 수 없는 플레이리스트";
+            return "제목을 찾을 수 없음";
         }
     }
 
@@ -389,7 +389,7 @@ public class PlaylistServiceImpl implements PlaylistService {
                     .getAsString();
             return title != null ? title : "Undefined";
         } catch (Exception e) {
-            return "Error fetching playlist title: " + e.getMessage();
+            return "제목을 찾을 수 없는 플레이리스트: " + e.getMessage();
         }
     }
 
